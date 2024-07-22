0909ef8e0414f911744664eed4751e5aa13ce552
Jani Sillanpää <janzku1993@hotmail.com>
2024-07-22 18:36:25 +0300

    init postgres db & entify framework with migrations

---
+++ BookApi/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookApi.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace YourNamespace;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.EntitiesToSnakeCase();
+    }
+
+    public DbSet<YourEntity> YourEntities { get; set; }
+}
+
+public class YourEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using YourNamespace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// Add DbContext with PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("BuuttiBookCS")));
+    
 
+var app = builder.Build();
+/*
+// Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+*/
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
