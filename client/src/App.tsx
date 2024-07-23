03eb3b6547245f70538469979cadeb8d69683303
Jani Sillanpää <janzku1993@hotmail.com>
2024-07-23 21:42:46 +0300

    client: env getter util, test fetch on backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,15 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
 import ErrorMessage from "@/components/ErrorMessage";
+import { useEffect } from "react";
+import { get } from "@/utilities/genericFetch";
+import ENV from "@/utilities/env";
 
 const App = () => {
+  useEffect(() => {
+    get(ENV.API_URL + "/book");
+  }, []);
+
   return (
     <Container>
       <Typography variant="h1" component="h2" gutterBottom>
---
+++ client/src/utilities/env.ts
@@ -0,0 +1,13 @@
+const getEnv = (key: string) => {
+  const envVariable = import.meta.env[key];
+
+  if (!envVariable) {
+    throw new Error(`Env variable ${key} not found`);
+  }
+
+  return envVariable as string;
+};
+
+export default {
+  API_URL: getEnv("VITE_APP_API_URL"),
+};
