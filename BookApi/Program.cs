e5c9f11a4090f0c5f7b1c5862b78e9bc9c208197
Jani Sillanpää <janzku1993@hotmail.com>
2024-07-28 19:23:32 +0300

    dockerize backend, db, pgadmin tool. run migrations on backend start up

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -24,6 +24,23 @@ builder.Services.AddScoped<IBookMapper, BookMapper>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
+
 app.UseCors(options =>
 {
     options.AllowAnyOrigin()
